5c416eee26e2cf2184f940ba443356cd5180ea2d
pratas <pratas@ua.pt>
2018-04-11 14:56:38 +0100

    getBear

---
+++ GetBear.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# SRR518649 
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156150/SRR518649.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156150/SRR518649_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156150/SRR518649_2.fastq.bz2
+#
+# SRR518651
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156143/SRR518651.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156143/SRR518651_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156143/SRR518651_2.fastq.bz2
+#
+# SRR518654
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156145/SRR518654.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156145/SRR518654_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156145/SRR518654_2.fastq.bz2
+#
+# SRR518655
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156146/SRR518655.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156146/SRR518655_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156146/SRR518655_2.fastq.bz2
+#
+# SRR518656
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156147/SRR518656.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156147/SRR518656_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156147/SRR518656_2.fastq.bz2
+#
+# SRR518657 
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156148/SRR518657.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156148/SRR518657_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156148/SRR518657_2.fastq.bz2
+#
+# SRR518704 
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156151/SRR518704.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156151/SRR518704_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156151/SRR518704_2.fastq.bz2
+#
+# SRR518705
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156152/SRR518705.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156152/SRR518705_1.fastq.bz2
+wget ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA054/SRA054912/SRX156152/SRR518705_2.fastq.bz2
+#
+##
