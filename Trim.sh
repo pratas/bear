a03405af73f5cc62b8cf4150dfff553482249a4a
Diogo Pratas <pratas@ua.pt>
2018-05-21 14:44:46 +0100

    Update Trim.sh

--- Trim.sh
+++ Trim.sh
@@ -13,29 +13,23 @@ fi
 #==============================================================================
 if [[ "$RUN_AR" -eq "1" ]]; then
   #
-  ./AdapterRemoval --file1 SRR518649.fastq --basename SE_SRR518649 --trimns --trimqualities --minlength 20 --gzip
   ./AdapterRemoval --file1 SRR518649_1.fastq --file2 SRR518649_2.fastq --basename PE_SRR518649 --trimns --trimqualities --minlength 20 --gzip
   #
-  ./AdapterRemoval --file1 SRR518651.fastq --basename SE_SRR518651 --trimns --trimqualities --minlength 20 --gzip
   ./AdapterRemoval --file1 SRR518651_1.fastq --file2 SRR518651_2.fastq --basename PE_SRR518651 --trimns --trimqualities --minlength 20 --gzip
   #
-  ./AdapterRemoval --file1 SRR518654.fastq --basename SE_SRR518654 --trimns --trimqualities --minlength 20 --gzip
   ./AdapterRemoval --file1 SRR518654_1.fastq --file2 SRR518654_2.fastq --basename PE_SRR518654 --trimns --trimqualities --minlength 20 --gzip
   #
-  ./AdapterRemoval --file1 SRR518655.fastq --basename SE_SRR518655 --trimns --trimqualities --minlength 20 --qualitybase 64 --gzip
-  ./AdapterRemoval --file1 SRR518655_1.fastq --file2 SRR518655_2.fastq --basename PE_SRR518655 --trimns --trimqualities --minlength 20 --qualitybase 64 --gzip
-  #
-  ./AdapterRemoval --file1 SRR518656.fastq --basename SE_SRR518656 --trimns --trimqualities --minlength 20 --gzip
   ./AdapterRemoval --file1 SRR518656_1.fastq --file2 SRR518656_2.fastq --basename PE_SRR518656 --trimns --trimqualities --minlength 20 --gzip
   #
-  ./AdapterRemoval --file1 SRR518657.fastq --basename SE_SRR518657 --trimns --trimqualities --minlength 20 --gzip
   ./AdapterRemoval --file1 SRR518657_1.fastq --file2 SRR518657_2.fastq --basename PE_SRR518657 --trimns --trimqualities --minlength 20 --gzip
   #
-  ./AdapterRemoval --file1 SRR518704.fastq --basename SE_SRR518704 --trimns --trimqualities --minlength 20 --gzip
+  ./AdapterRemoval --file1 SRR518659_1.fastq --file2 SRR518659_2.fastq --basename PE_SRR518659 --trimns --trimqualities --minlength 20 --gzip
+  #
   ./AdapterRemoval --file1 SRR518704_1.fastq --file2 SRR518704_2.fastq --basename PE_SRR518704 --trimns --trimqualities --minlength 20 --gzip
   # 
-  ./AdapterRemoval --file1 SRR518705.fastq --basename SE_SRR518705 --trimns --trimqualities --minlength 20 --gzip
   ./AdapterRemoval --file1 SRR518705_1.fastq --file2 SRR518705_2.fastq --basename PE_SRR518705 --trimns --trimqualities --minlength 20 --gzip
+  #
+  ./AdapterRemoval --file1 SRR518706_1.fastq --file2 SRR518706_2.fastq --basename PE_SRR518706 --trimns --trimqualities --minlength 20 --gzip
   #
   zcat *.truncated.gz > PUM.fq
 fi
