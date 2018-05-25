0fa1fe8957090ec66729da5d8f7de1dd0e932a62
Diogo Pratas <pratas@ua.pt>
2018-05-25 12:02:28 +0100

    part 1!!!

--- DownloadMTV2.sh
+++ DownloadMTV2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -f mitochondrion.2.1.genomic.fna.gz;
+rm -f mitochondrion.1.1.genomic.fna.gz mitochondrion.2.1.genomic.fna.gz;
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.1.1.genomic.fna.gz
 wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.2.1.genomic.fna.gz
-zcat mitochondrion.2.1.genomic.fna.gz > mito.fna
+zcat mitochondrion.1.1.genomic.fna.gz mitochondrion.2.1.genomic.fna.gz > mito.fna
