7d5bd3b958c9e0dd92160b7be1ea4025bf21e2ca
Diogo Pratas <pratas@ua.pt>
2018-07-02 16:09:28 +0100

    run individual

---
+++ runIndividual.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+(time ./FALCON -v -n 1 -t 800 -F -Z -c 250 -y complexity-mt.com -x top-mt.csv PUM.fq /home/pratas/DATABASE/DB/abril_27_2018/mito.fna ) &> REPORT-MT_FALCON
+./FALCON-FILTER -v -F -sl 0.001 -du 20000000 -t 0.5 -o positions-mt.pos complexity-mt.com 
+./FALCON-EYE -v -F -e 500 -sl 8 -o draw-mt.svg positions-mt.pos
+#
+(time ./FALCON -v -n 1 -t 800 -F -Z -c 250 -y complexity-pl.com -x top-pl.csv PUM.fq /home/pratas/DATABASE/DB/abril_27_2018/plast.fna ) &> REPORT-PL_FALCON
+./FALCON-FILTER -v -F -sl 0.001 -du 20000000 -t 0.5 -o positions-pl.pos complexity-pl.com
+./FALCON-EYE -v -F -e 500 -sl 8 -o draw-pl.svg positions-pl.pos
+#
+(time ./FALCON -v -n 8 -t 800 -F -Z -c 250 -y complexity-v.com -x top-v.csv PUM.fq /home/pratas/DATABASE/DB/abril_27_2018/viruses.fa ) &> REPORT-V_FALCON
+./FALCON-FILTER -v -F -sl 0.001 -du 20000000 -t 0.5 -o positions-v.pos complexity-v.com
+./FALCON-EYE -v -F -e 500 -sl 8 -o draw-v.svg positions-v.pos
+#
+(time ./FALCON -v -n 8 -t 800 -F -Z -c 250 -y complexity-a.com -x top-a.csv PUM.fq /home/pratas/DATABASE/DB/abril_27_2018/f-archaea.fa ) &> REPORT-A_FALCON
+./FALCON-FILTER -v -F -sl 0.001 -du 20000000 -t 0.5 -o positions-a.pos complexity-a.com
+./FALCON-EYE -v -F -e 500 -sl 8 -o draw-a.svg positions-a.pos
+#
+(time ./FALCON -v -n 8 -t 800 -F -Z -c 250 -y complexity-b.com -x top-b.csv PUM.fq /home/pratas/DATABASE/DB/abril_27_2018/f-bacteria.fa ) &> REPORT-B_FALCON
+./FALCON-FILTER -v -F -sl 1 -du 20000000 -t 0.5 -o positions-b.pos complexity-b.com
+./FALCON-EYE -v -F -e 500 -sl 9.3 -o draw-b.svg positions-b.pos
+#
+(time ./FALCON -v -n 8 -t 800 -F -Z -c 250 -y complexity-f.com -x top-f.csv PUM.fq /home/pratas/DATABASE/DB/abril_27_2018/f-fungi.fa ) &> REPORT-F_FALCON
+./FALCON-FILTER -v -F -sl 0.001 -du 20000000 -t 0.5 -o positions-f.pos complexity-f.com
+./FALCON-EYE -v -F -e 500 -sl 8 -o draw-f.svg positions-f.pos
+#
